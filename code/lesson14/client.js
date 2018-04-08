4b55cb90a4958edcdb1a1a057ef8bc40afd3ee89
[LukeMi] <[826861729@qq.com]>
2018-04-09 07:47:02 +0800

    【描述】 添加视频链接测试

--- code/lesson14/client.js
+++ code/lesson14/client.js
@@ -1,22 +1,25 @@
 var http = require('http');
-
-var url = require('url');
-
-var option = {
-    host : 'localhost',
-    port : '8080',
-    path : 'index.html'
-}
-
+ 
+// 用于请求的选项
+var options = {
+   host: 'localhost',
+   port: '9080',
+   path: '/index.html'  
+};
+ 
+// 处理响应的回调函数
 var callback = function(response){
-    var body ='';
-    response.on('data',function(data){
-        body += data;
-    });
-    response.on('end',function(){
-        console.log('数字据接受完成');
-    })
+   // 不断更新数据
+   var body = '';
+   response.on('data', function(data) {
+      body += data;
+   });
+   
+   response.on('end', function() {
+      // 数据接收完成
+      console.log(body);
+   });
 }
-
-var req = http.request(option,callback);
+// 向服务端发送请求
+var req = http.request(options, callback);
 req.end();
