02cae074201908e261e70640c0e6ad3ba7bce0ae
[LukeMi] <[826861729@qq.com]>
2018-04-18 00:06:31 +0800

    【描述】 新增supervisor 工具 便于调试  【提交人】  陈明智
    Signed-off-by: [LukeMi] <[826861729@qq.com]>

---
+++ code/lesson1/console.js
@@ -0,0 +1,8 @@
+console.log("hello world");
+console.log("hello world"," sub");
+console.log("hello %s world"," sub");
+console.log("hello %s %s world"," first"," second");
+console.log("hello %d %s world",2333," second");
+
+
+
--- code/lesson7/http.js
+++ code/lesson7/http.js
@@ -2,8 +2,8 @@ var http = require('http');
 
 
 function onRequest(request,response) {
-    response.writeHead(200, { "Content-Type": "text/plain" });
-    response.write("Hello World ,你是我的小苹果");
+    response.writeHead(200, { "Content-Type": "text/plain;charset=utf-8" });
+    response.write("Hello World ,你是我的小苹果,222222222");
     response.end();
 }
 
